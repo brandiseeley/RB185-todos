bd2bea620b1d97a31b6b337549a118a4559f4aa4
Brandi Seeley <brandi.n.seeley@gmail.com>
2023-02-14 17:58:07 -0500

    Set session secret to hex32

--- todo.rb
+++ todo.rb
@@ -2,10 +2,11 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/content_for"
 require "tilt/erubis"
+require 'securerandom'
 
 configure do
   enable :sessions
-  set :session_secret, 'secret'
+  set :session_secret, SecureRandom.hex(32)
   set :erb, :escape_html => true
 end
 
